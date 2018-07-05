f604270b1737412370ce1e80f203c0d470a39723
mixn <sutanovac.milos@gmail.com>
2018-07-06 01:42:00 +0200

    ✅ Adds more dummies and tests

--- test/process-content.test.js
+++ test/process-content.test.js
@@ -8,14 +8,29 @@ import test from 'ava';
 // Source
 import process from '../src/process-content';
 
-test('Correctly processes entire content', async t => {
-	try {
-		const readFileAsync = promisify(fs.readFile); // Fixing Ryan’s mistakes… *sigh* https://goo.gl/fMr85g JK gr8 talk m8 I r8 8/8 👍
-		const expected = await readFileAsync('./test/test-dummies/_unfold.js', {
-			encoding: 'utf8'
-		});
+const readFileAsync = promisify(fs.readFile); // Fixing Ryan’s mistakes… *sigh* https://goo.gl/fMr85g JK gr8 talk m8 I r8 8/8 👍
+const readFileAsyncEncoded = file => readFileAsync(file, {
+	encoding: 'utf8'
+});
 
-		t.is(await process('./test/test-dummies/_unfold.js'), expected);
+test('Correctly processes full length of files', async t => {
+	const [js, rust, html] = [
+		'./test/test-dummies/_unfold.js',
+		'./test/test-dummies/_main.rs',
+		'./test/test-dummies/_index.html'
+	];
+	let expected;
+
+	try {
+		// TODO: Make this better
+		expected = await readFileAsyncEncoded(js);
+		t.is(await process(js), expected);
+
+		expected = await readFileAsyncEncoded(rust);
+		t.is(await process(rust), expected);
+
+		expected = await readFileAsyncEncoded(html);
+		t.is(await process(html), expected);
 	} catch (error) {
 		t.fail();
 	}
