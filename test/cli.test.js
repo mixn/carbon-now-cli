51ab9ac4a97033d2373f486ba6dadf434d1fcd3e
mixn <sutanovac.milos@gmail.com>
2018-07-05 19:57:01 +0200

    ✅ cli.test.js: Excplicitly fails test
    Instead of implicitly failing due to missing assertion.

--- test/cli.test.js
+++ test/cli.test.js
@@ -5,6 +5,7 @@ import execa from 'execa';
 test('Running `carbon-now-sh` fails without at least one argument', async t => {
 	try {
 		await execa.stdout('./cli.js');
+		t.fail();
 	} catch (error) {
 		t.is(error.failed, true);
 	}
