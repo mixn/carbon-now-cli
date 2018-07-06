dcf110fc470698f8b353b2b1a621ed3fcc1680e6
mixn <sutanovac.milos@gmail.com>
2018-07-06 21:39:44 +0200

    ✅ Adds headless-visit.test.js

---
+++ test/headless-visit.test.js
@@ -0,0 +1,39 @@
+// Packages
+import test from 'ava';
+import globby from 'globby';
+import del from 'del';
+
+// Source
+import headlessVisit from '../src/headless-visit';
+
+test('Fails due to wrong URL/timeout/event', async t => {
+	try {
+		await headlessVisit('foobar');
+		t.fail();
+	} catch (error) {
+		t.pass();
+	}
+});
+
+test('Downloads code image correctly', async t => {
+	const defaultDownloadName = 'carbon.png';
+
+	try {
+		// Glob for a potentially existing download
+		const globbed = await globby([defaultDownloadName]);
+
+		// If a download exists, delete it
+		// XO makes me check like this ¯\_(ツ)_/¯
+		if (globbed.length > 0) {
+			await del(globbed);
+		}
+
+		// Download image
+		await headlessVisit('https://carbon.now.sh');
+
+		// If it exists, pass
+		t.true((await globby([defaultDownloadName])).length > 0);
+	} catch (error) {
+		t.fail();
+	}
+});
