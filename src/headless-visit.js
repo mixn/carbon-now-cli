a2d1e9b57f5420f31e40d0e931411e685e628b85
Sneezoo <blue.xdimension@gmail.com>
2018-07-13 20:40:24 +0200

    Add workaround avoiding overriding downloadBehavior

--- src/headless-visit.js
+++ src/headless-visit.js
@@ -1,11 +1,13 @@
 // Packages
 const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = async (url, location = process.cwd(), type = 'png', disableSandbox = false) => {
 	// Launch browser
 	const browser = await puppeteer.launch({
 		headless: false,
-		args: disableSandbox ? ['--no-sandbox', '--disable-setuid-sandbox'] : []
+		args: disableSandbox ? ['--no-sandbox', '--disable-setuid-sandbox'] : [],
 	});
 	// Open new page
 	const page = await browser.newPage();
@@ -27,21 +29,63 @@ module.exports = async (url, location = process.cwd(), type = 'png', disableSand
 		downloadPath: `${location}/`
 	});
 
-	// `page.waitForSelector` https://goo.gl/gGLKBL ➝ exactly what I needed 👍
-	const saveImageTrigger = await page.waitForSelector('[aria-labelledby="downshift-2-label"]');
-	// Only after this is clicked, the png and svg triggers will exist in the DOM
-	await saveImageTrigger.click();
+	await page.addScriptTag({
+		path: path.resolve(__dirname,
+			'..', 
+			'node_modules', 
+			'dom-to-image', 
+			'src', 
+			'dom-to-image.js')
+	});
 
-	const pngExportTrigger = await page.$('#downshift-2-item-0');
-	const svgExportTrigger = await page.$('#downshift-2-item-1');
+	await page.evaluate(() => {
+		if (typeof domtoimage === 'undefined' && module.exports) {
+			window.domtoimage = module.exports;
+		}
+	});
+	
+	const dataUrl = await page.evaluate(() => {
+    const node = document.getElementById('export-container')
 
-	if (type === 'png') {
-		await pngExportTrigger.click();
-	} else if (type === 'svg') {
-		await svgExportTrigger.click();
-	} else {
-		throw new Error('Only png and svg are supported.');
-	}
+		// EXPORT SIZES=1,2,4
+    const exportSize = 2;
+    const width = node.offsetWidth * exportSize;
+    const height = false //true for squared image
+      ? node.offsetWidth * exportSize
+      : node.offsetHeight * exportSize;
+
+    const config = {
+      style: {
+        transform: `scale(${exportSize})`,
+        'transform-origin': 'center',
+        background: 'none' //this.state.squaredImage ? this.state.backgroundColor : 'none'
+      },
+      filter: n => {
+        // %[00 -> 19] cause failures
+        if (
+          n.innerText && n.innerText.match(/%[0-1][0-9]/) &&
+          n.className &&
+          n.className.startsWith('cm-') // is CodeMirror primitive string
+        ) {
+          n.innerText = n.innerText.replace('%', '%25')
+        }
+        if (n.className) {
+          return String(n.className).indexOf('eliminateOnRender') < 0
+        }
+        return true
+      },
+      width,
+      height
+    }
+    // we need regular dataurls
+    return window.domtoimage.toPng(node, config)
+	});
+
+	console.log('fetched data url');
+	const data = dataUrl.slice("data:image/png;base64,".length)
+	const buffer = new Buffer(data, 'base64');
+	fs.writeFileSync('carbon.png', buffer);
+	console.log('wrote image to carbon.png');
 
 	// Wait some more as `waitUntil: 'load'` or `waitUntil: 'networkidle0'
 	// is not always enough, see https://goo.gl/eTuogd
