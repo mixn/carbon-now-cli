5bc24b779ca4ca750543e094f63087b0d4939560
mixn <sutanovac.milos@gmail.com>
2018-07-14 14:30:45 +0200

    🚧 preset.js: Uses globals + allows setting a different config

--- src/preset.js
+++ src/preset.js
@@ -1,31 +1,29 @@
-// Native
-const {homedir} = require('os');
-
 // Packages
 const fileExists = require('file-exists');
 const jsonFile = require('jsonfile');
 const {omit} = require('lodash');
 
-const CONFIG_FILE = `${homedir()}/.carbon-now.json`;
+// Source
+const {FULL_CONFIG_PATH} = require('./helpers/globals');
 
 module.exports = {
-	async save(presetName, settings) {
+	async save(presetName, settings, config = FULL_CONFIG_PATH) {
 		let currentConfig = {};
 
 		try {
-			if (await fileExists(CONFIG_FILE)) {
+			if (await fileExists(config)) {
 				// We want to remember the current config if one exsists
-				currentConfig = await jsonFile.readFileSync(CONFIG_FILE);
+				currentConfig = await jsonFile.readFileSync(config);
 			}
 
 			// This will create the file if it doesn’t exist
-			// and append to it an existing config, if it does
+			// or append to it an existing config, if it does
 			await jsonFile.writeFileSync(
-				CONFIG_FILE,
+				config,
 				{
 					...currentConfig,
 					[presetName]: {
-						...omit(settings, ['save', 'preset'])
+						...omit(settings, ['save', 'preset', 'l'])
 					},
 					'last-used-preset': presetName
 				},
