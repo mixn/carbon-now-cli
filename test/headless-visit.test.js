090f9d109779a146356029f1d404e31cfbc4a19e
mixn <sutanovac.milos@gmail.com>
2018-08-19 13:59:20 +0200

    🎨 headless-visit.test.js: Adapts overseen constants convention

--- test/headless-visit.test.js
+++ test/headless-visit.test.js
@@ -6,9 +6,9 @@ import del from 'del';
 // Source
 import headlessVisit from '../src/headless-visit';
 
-const defaultDownloadName = 'carbon.png';
-const downloadDir = 'carbon';
-const fullDownloadPath = `${downloadDir}/${defaultDownloadName}`;
+const DEFAULT_DOWNLOAD_NAME = 'carbon.png';
+const DOWNLOAD_DIR = 'carbon';
+const FULL_DOWNLOAD_PATH = `${DOWNLOAD_DIR}/${DEFAULT_DOWNLOAD_NAME}`;
 
 test('Fails due to wrong URL/timeout/event', async t => {
 	try {
@@ -22,7 +22,7 @@ test('Fails due to wrong URL/timeout/event', async t => {
 test('Downloads code image correctly', async t => {
 	try {
 		// Glob for a potentially existing download
-		const globbed = await globby([defaultDownloadName]);
+		const globbed = await globby([DEFAULT_DOWNLOAD_NAME]);
 
 		// If a download exists, delete it
 		// XO makes me check like this ¯\_(ツ)_/¯
@@ -34,7 +34,7 @@ test('Downloads code image correctly', async t => {
 		await headlessVisit('https://carbon.now.sh');
 
 		// If it exists, pass
-		t.true((await globby([defaultDownloadName])).length > 0);
+		t.true((await globby([DEFAULT_DOWNLOAD_NAME])).length > 0);
 	} catch (error) {
 		t.fail();
 	}
@@ -42,7 +42,7 @@ test('Downloads code image correctly', async t => {
 
 test('Downloads code image correctly with `headless` set', async t => {
 	try {
-		const globbed = await globby([defaultDownloadName]);
+		const globbed = await globby([DEFAULT_DOWNLOAD_NAME]);
 
 		if (globbed.length > 0) {
 			await del(globbed);
@@ -52,22 +52,22 @@ test('Downloads code image correctly with `headless` set', async t => {
 		await headlessVisit('https://carbon.now.sh', null, null, true);
 
 		// If it exists, pass
-		t.true((await globby([defaultDownloadName])).length > 0);
+		t.true((await globby([DEFAULT_DOWNLOAD_NAME])).length > 0);
 	} catch (error) {
 		t.fail();
 	}
 });
 
 test('Respects download location', async t => {
-	await headlessVisit('https://carbon.now.sh', downloadDir);
+	await headlessVisit('https://carbon.now.sh', DOWNLOAD_DIR);
 
-	t.true((await globby([fullDownloadPath])).length > 0);
+	t.true((await globby([FULL_DOWNLOAD_PATH])).length > 0);
 });
 
 // Cleanup
 test.after.always((async () => {
 	await del([
-		defaultDownloadName,
-		downloadDir
+		DEFAULT_DOWNLOAD_NAME,
+		DOWNLOAD_DIR
 	]);
 }));
