cc432784a639a08c57102a84aa5eb0f7a2173ed4
mixn <sutanovac.milos@gmail.com>
2018-08-20 14:01:49 +0200

    🐛 cli.js: Fixes undefined TERM_PROGRAM case

--- cli.js
+++ cli.js
@@ -193,7 +193,7 @@ if (!file) {
   The file can be found here: ${savedAs} 😌`
 				);
 
-				if (process.env.TERM_PROGRAM.match('iTerm')) {
+				if (process.env.TERM_PROGRAM && process.env.TERM_PROGRAM.match('iTerm')) {
 					console.log(`
   iTerm2 should display the image below. 😊
 
