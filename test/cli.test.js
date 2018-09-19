60766c83be4218084685fd6117829d1d752ca3bb
mixn <sutanovac.milos@gmail.com>
2018-09-19 17:21:34 +0200

    ✅ cli.test.js: Adds test for --target

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,8 +1,13 @@
 // Packages
 import test from 'ava';
 import execa from 'execa';
+import fileExists from 'file-exists';
+import del from 'del';
 
-test('Running `carbon-now` fails without at least one argument', async t => {
+const DUMMY_TARGET = 'custom-file-name';
+const DUMMY_FILE = `${DUMMY_TARGET}.png`;
+
+test.serial('Running `carbon-now` fails without at least one argument', async t => {
 	try {
 		await execa.stdout('./cli.js');
 		t.fail();
@@ -10,3 +15,19 @@ test('Running `carbon-now` fails without at least one argument', async t => {
 		t.true(error.failed);
 	}
 });
+
+test.serial('Allows for custom file name via --target', async t => {
+	await execa('./cli.js', [
+		'./test/test-dummies/_unfold.js',
+		`-t=${DUMMY_TARGET}`
+	]);
+
+	t.true(await fileExists(DUMMY_FILE));
+});
+
+// Cleanup
+test.after.always((async () => {
+	await del([
+		DUMMY_FILE
+	]);
+}));
