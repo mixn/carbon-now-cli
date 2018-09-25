7c27fed05e37af9596bef7361ddfab255e798981
mixn <sutanovac.milos@gmail.com>
2018-09-25 03:03:29 +0200

    ✅ cli.test.js: --config related tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -4,12 +4,19 @@ import execa from 'execa';
 import fileExists from 'file-exists';
 import del from 'del';
 
-const DUMMY_TARGET = 'custom-file-name';
-const DUMMY_FILE = `${DUMMY_TARGET}.png`;
+// Util
+import readfileAsync from '../src/util/readfile-async';
+
+const SCRIPT = './cli.js';
+const DUMMY_FROM = './test/test-dummies/_unfold.js';
+const DUMMY_TARGET_NAME = 'custom-file-name';
+const DUMMY_FILE_NAME = `${DUMMY_TARGET_NAME}.png`;
+const ABSENT_DUMMY_CONFIG = './non-existent.json';
+const PRESENT_DUMMY_CONFIG = './test/test-dummies/_config.json';
 
 test.serial('Running `carbon-now` fails without at least one argument', async t => {
 	try {
-		await execa.stdout('./cli.js');
+		await execa.stdout(SCRIPT);
 		t.fail();
 	} catch (error) {
 		t.true(error.failed);
@@ -17,17 +24,41 @@ test.serial('Running `carbon-now` fails without at least one argument', async t 
 });
 
 test.serial('Allows for custom file name via --target', async t => {
-	await execa('./cli.js', [
-		'./test/test-dummies/_unfold.js',
-		`-t=${DUMMY_TARGET}`
+	await execa(SCRIPT, [
+		DUMMY_FROM,
+		`-t=${DUMMY_TARGET_NAME}`
 	]);
 
-	t.true(await fileExists(DUMMY_FILE));
+	t.true(await fileExists(DUMMY_FILE_NAME));
+});
+
+test.serial('Doesn’t create config when --config provided', async t => {
+	await execa(SCRIPT, [
+		DUMMY_FROM,
+		`--config=${ABSENT_DUMMY_CONFIG}`,
+		`-t=${DUMMY_TARGET_NAME}`
+	]);
+
+	t.false(await fileExists(ABSENT_DUMMY_CONFIG));
+});
+
+test.serial('Doesn’t modify local config, treats it as read-only', async t => {
+	const BEFORE = await readfileAsync(PRESENT_DUMMY_CONFIG);
+
+	await execa(SCRIPT, [
+		DUMMY_FROM,
+		`--config=${PRESENT_DUMMY_CONFIG}`,
+		`-t=${DUMMY_TARGET_NAME}`
+	]);
+
+	const AFTER = await readfileAsync(PRESENT_DUMMY_CONFIG);
+
+	t.is(BEFORE, AFTER);
 });
 
 // Cleanup
-test.after.always((async () => {
+test.afterEach((async () => {
 	await del([
-		DUMMY_FILE
+		DUMMY_FILE_NAME
 	]);
 }));
