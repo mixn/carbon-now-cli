7f7081906fd40532a42504b9460189b0d86e5241
mixn <sutanovac.milos@gmail.com>
2023-03-26 22:16:18 +0200

    🚧 Bumps execa, husky versions, sets up husky

--- test/utils/read-input.util.test.ts
+++ test/utils/read-input.util.test.ts
@@ -5,15 +5,18 @@ import readFileAsync from '../../src/utils/read-file-async.util.js';
 const DUMMY_FILE = './test/test-dummies/_unfold.js';
 const DUMMY_INPUT = '<owen-wilson says="Wow" />';
 
-test('Reads file content correctly when input source is <file>', async () => {
+test('Should read file content correctly when input source is <file>', async () => {
 	expect(await readInput(DUMMY_FILE)).toBe(await readFileAsync(DUMMY_FILE));
 });
 
-test('Reads from clipboard correctly when --from-clipboard flag is set', async () => {
+test('Should read from clipboard correctly when --from-clipboard flag is set', async () => {
 	clipboardy.writeSync(DUMMY_INPUT);
 	expect(await readInput(undefined, true)).toBe(clipboardy.readSync());
 });
 
-test('Throws an error when no input source exists', async () => {
+test('Should reject with correct message when no input source exists', async () => {
 	await expect(readInput(undefined)).rejects.toEqual('No file or stdin given.');
 });
+
+// TODO: Should read from stdin correctly when neither <file> nor --from-clipboard flag are present
+// https://github.com/caitp/node-mock-stdin
